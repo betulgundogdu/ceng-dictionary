6b6d50afea50c3dadbc82318550e05c34fa3c131
betulg <betul.gundogdu@ceng.deu.edu.tr>
2021-01-08 01:55:59 +0300

    adding entry & category queries are added

--- components/entry.php
+++ components/entry.php
@@ -1,49 +1,101 @@
+<?php
+    include('./connect.php');
+    $category = $_GET['category'];
+    if($category == ""){
+        $category = 1;
+    }
+    $date = date('Y-m-d');
+    $u_id = $_SESSION["id"];
+    $h_id = $_GET['baslik'];
+    $h_title = '';
+    $stmt = $mysqli->prepare("SELECT h_id, title FROM Header WHERE c_id = ?");
+    $stmt->bind_param("s", $category);
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        if (count($result) > 0) {
+            while($row = $result -> fetch_assoc()) {
+                $h_id = $row['h_id'];
+                $h_title = $row['title'];
+            }
+        } else {    
+            echo "0 results";
+        }  
+        $stmt->close();    
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+?>
+
 <div class="title"> 
-    <div>deneme bir kideneme bir ki</div>
+    <div><?php echo $h_title ?></div>
     <a class="edit"> düzenle </a>|<a class="edit"> sil</a> <!-- sadece mod/admin-->
 </div>
 
 <div class="entries">
     <div class="entry">
-        <p class="entry-content"> Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus.</p>
+        <p class="entry-content"> hello world</p>
         <div class="right detail">
             <span class="username">username</span><span class="time"> | 06.03.23 23:21</span>
             <span>| <a class="edit">delete </a></span> <!--mod ve admin-->
         </div>
     </div>
-    <div class="entry">
-        <p class="entry-content"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus.</p>
-        <div class="right detail">
-            <span class="username">username</span><span class="time"> | 06.03.23 23:21</span>
-            <span> | <a class="edit">delete </a></span>
-        </div>
-    </div>
-    <div class="entry">
-        <p class="entry-content"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus.</p>
-        <div class="right detail">
-            <span class="username">username</span><span class="time"> | 06.03.23 23:21</span>
-            <span> | <a class="edit">delete </a></span>
-        </div>
-    </div>
-    <div class="entry">
-        <p class="entry-content"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin ligula vitae ante pretium auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis aliquam auctor eros, eget lacinia tortor ornare non. Nunc mauris mi, tristique eget consequat quis, convallis a neque. Vivamus fermentum vehicula ligula, et hendrerit justo. Mauris ante lorem, sodales vitae lacus cursus, bibendum varius neque. Nunc metus turpis, bibendum at egestas et, sodales fringilla metus.</p>
-        <div class="right detail">
-            <span class="username">username</span><span class="time"> | 06.03.23 23:21</span>
-            <span> | <a class="edit">delete </a></span>
-        </div>
-    </div>
-    <?php
-        session_start();
+<?php
+    $stmt = $mysqli->prepare("SELECT text, created_date, u_id FROM Entry WHERE h_id = ?");
+    $stmt->bind_param("s", $h_id);
+    if ($stmt->execute()) {
+        $result = $stmt->get_result();
+        if (count($result) > 0) {
+            while($row = $result -> fetch_assoc()) {
+                echo(" 
+                    <div class='entry'>
+                        <p class='entry-content'>" . $row['text'] . "</p>
+                        <div class='right detail'>
+                            <span class='username'>" . $row['u_id']. "</span>
+                            <span class='time'> | " . $row['created_date'] . "</span>
+                            <span> | <a class='edit'>delete </a></span> <!--mod ve admin-->
+                        </div>
+                    </div>                     
+                ");
+            }
+        } else {    
+            echo "0 results";
+        }  
+        $stmt->close();    
+    } else {
+        echo "Error: " . $stmt->error;
+    }
 
-        if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == true){
-            echo '
-            <form class="entry-form" action="/add-entry.php">  <!-- sadece üyeler-->
-                <label for="lname">entry gir:</label><br/>
-                <textarea id="entry" name="new-entry" rows="10" cols="30"></textarea><br/>
-                <input type="submit" value="gönder"/>
-            </form> '; 
+    //Form for adding entry 
+    session_start();
+    if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == true){
+        echo '
+        <form class="entry-form" method="POST">  <!-- sadece üyeler-->
+            <label for="lname">entry gir:</label><br/>
+            <textarea id="entry" name="entry" rows="10" cols="30"></textarea><br/>
+            <input type="submit" value="gönder"/>
+        </form> '; 
+    }
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(!empty($h_id)){
+            if(empty(trim($_POST["entry"]))){
+                $text_err = "Please enter a text.";
+            }
+            else {
+                $entry = trim($_POST["entry"]);
+                $stmt = $mysqli->prepare("INSERT INTO Entry (h_id, text, created_date, u_id) VALUES (?, ?, ?, ?)");
+                $stmt->bind_param("ssss", $h_id , $entry, $date, $u_id);
+                if ($stmt->execute()) {
+                    echo "New record created successfully";
+                    header("location: index.php?page=entry&baslik=". $h_id);
+                } else {
+                    echo "Error: " . $stmt->error;
+                }       
+                $stmt->close();
+            }
         }
-    ?>
+    }
+    $mysqli->close();               
+?>
 </div>
 
 <div class="page-count">
--- components/left-section.php
+++ components/left-section.php
@@ -1,9 +1,15 @@
+<?php
+    include('./connect.php');
+    $category = $_GET['category'];
+    if($category == ""){
+        $category = 1;
+    }
+?>
+
 <div class="left container">
     <div class="left wrapper">
         <div class="categories">
             <?php 
-                include('./connect.php');
-
                 $sql = "SELECT c_id, title FROM Category";
                 $result = $mysqli -> query($sql);
 
@@ -18,7 +24,6 @@
                 } else {    
                     echo "0 results";
                 }      
-                $mysqli->close();        
             ?>
             <div class="item"><a href="category.html"> daha fazla </a></div>
             <div class="item"><button type="submit"><i class="fa fa-search"></i></button></div>
@@ -46,18 +51,14 @@
         </div>
         <ul class="subjects">
             <?php 
-                include('./connect.php');
-                $category = $_GET['category'];
-                if($category == ""){
-                    $category = 1;
-                }
+
                 $stmt = $mysqli->prepare("SELECT h_id, title FROM Header WHERE c_id = ?");
                 $stmt->bind_param("s", $category);
                 if ($stmt->execute()) {
                     $result = $stmt->get_result();
                     if (count($result) > 0) {
                         while($row = $result -> fetch_assoc()) {
-                            echo  "<li class='item'><a href='?page=entry&entry=". $row['h_id'] . "'>" . $row['title'] . "</a></li>";
+                            echo  "<li class='item'><a href='?page=entry&baslik=". $row['h_id'] . "'>" . $row['title'] . "</a></li>";
                         }
                     } else {    
                         echo "0 results";
@@ -65,30 +66,10 @@
                     $stmt->close();    
                     $mysqli->close();               
                 } else {
-                    echo "Error: " . $this->stmt->error;
+                    echo "Error: " . $stmt->error;
                 }
-
-      
             ?>
-            <!-- <li class="item"><a href="entry.php"> deneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> #deneme bir kideneme bir ki</a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki deneme bir kideneme bir ki  </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki  </a></li>
-            <li class="item"><a href="entry.php"> #deneme bir kideneme bir ki  </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki  </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki deneme bir kideneme bir ki  </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir ki </a></li>
-            <li class="item"><a href="entry.php"> deneme bir kideneme bir ki </a></li> 
-            <li class="item more"><a href="#"> daha fazla  </a></li> -->
+
         </ul>
     </div>
 </div>
