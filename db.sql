a5862aa4d7d5d3dc0a61bb971676bd685d088c08
betulg <betul.gundogdu@ceng.deu.edu.tr>
2021-01-19 21:40:52 +0300

    db edited

--- db.sql
+++ db.sql
@@ -12,7 +12,11 @@ CREATE TABLE `Header` (
   `c_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`h_id`),
   KEY `Header_FK` (`c_id`),
-  CONSTRAINT `Header_FK` FOREIGN KEY (`c_id`) REFERENCES `Category` (`c_id`)
+  CONSTRAINT `Header_FK`
+    FOREIGN KEY (`c_id`)
+    REFERENCES `Category` (`c_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `Entry` (
@@ -24,8 +28,16 @@ CREATE TABLE `Entry` (
   PRIMARY KEY (`e_id`),
   KEY `Entry_FK` (`u_id`),
   KEY `Entry_FK_1` (`h_id`),
-  CONSTRAINT `Entry_FK` FOREIGN KEY (`u_id`) REFERENCES `User` (`u_id`),
-  CONSTRAINT `Entry_FK_1` FOREIGN KEY (`h_id`) REFERENCES `Header` (`h_id`)
+  CONSTRAINT `Entry_FK` 
+    FOREIGN KEY (`u_id`) 
+    REFERENCES `User` (`u_id`) 
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `Entry_FK_1`
+    FOREIGN KEY (`h_id`)
+    REFERENCES `Header` (`h_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE `User` (
@@ -46,7 +58,29 @@ CREATE TABLE `Message` (
   PRIMARY KEY (`m_id`),
   KEY `Message_FK` (`sender_id`),
   KEY `Message_FK_1` (`receiver_id`),
-  CONSTRAINT `Message_FK` FOREIGN KEY (`sender_id`) REFERENCES `User` (`u_id`),
-  CONSTRAINT `Message_FK_1` FOREIGN KEY (`receiver_id`) REFERENCES `User` (`u_id`)
+  CONSTRAINT `Message_FK` 
+    FOREIGN KEY (`sender_id`) 
+    REFERENCES `User` (`u_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `Message_FK_1` 
+    FOREIGN KEY (`receiver_id`) 
+    REFERENCES `User` (`u_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
+INSERT INTO ceng_dict.Category (title) VALUES
+	 ('genel'),
+	 ('os'),
+	 ('embedded'),
+	 ('ai / ml'),
+	 ('networks'),
+	 ('web'),
+	 ('mobil');
+
+INSERT INTO ceng_dict.Header (title,c_id) VALUES
+	 ('hello world',1);
+
+INSERT INTO ceng_dict.User (username,password,created_at,type) VALUES
+	 ('admin',123, 19-01-2021, 'admin');
